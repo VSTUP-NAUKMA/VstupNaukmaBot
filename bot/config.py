edf068d0ecdbd6f896098b68b508b63a310d37d9
Yevhen Anisimov <yevhen.anisimov@ukma.edu.ua>
2023-11-18 16:59:08 +0200

    Integrate logging functionality and enhance chat functionality

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,15 @@
+import logging
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 
 def load_env():
     env_path = Path('.') / '.env'
     load_dotenv(dotenv_path=env_path)
+
+
+logging.basicConfig(filename='Logs.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import asyncio
 import os
 
 from telegram import Update
-from telegram.ext import Application
+from telegram.ext import Application, CallbackQueryHandler
 
 from bot.config import load_env
-from bot.handlers.operator_chat import reply_handler
+from bot.handlers.operator_chat import reply_handler, button_callback, clear_pending_replies
 from bot.handlers.start import conv_handler
 
 if __name__ == '__main__':
     load_env()
     application = Application.builder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
+    button_handler = CallbackQueryHandler(button_callback)
+    application.add_handler(button_handler)
     application.add_handler(conv_handler)
     application.add_handler(reply_handler)
+    loop = asyncio.get_event_loop()
+    loop.create_task(clear_pending_replies(43200))  # 12 hours
     application.run_polling(allowed_updates=Update.ALL_TYPES)
