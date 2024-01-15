8e457721aa8b158e31baea587b1246dbf13feafd
Yevhen Anisimov <zhecky2004@gmail.com>
2024-01-15 19:58:58 +0200

    start refactor

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,30 +1,21 @@
 from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler, MessageHandler, filters
 
-from bot.handlers.dormitory import dormitory, BACK, go_home
-from bot.handlers.operator_chat import connect_with_operator
-from bot.handlers.admission import admission
-
-CHOOSING, DORMITORY, IN_CONVERSATION = range(3)
+CHOOSING = 1
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_keyboard = [['Вступ на навчання', 'Навчальний процес', 'Гуртожитки'],
                       ['Студентське життя', 'Контакти', 'Чат-підтримка']]
     keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
-    await update.message.reply_text("Hi!", reply_markup=keyboard_markup)
+    await update.message.reply_text("Привіт! Обери потрібний розділ", reply_markup=keyboard_markup)
 
 
 start_handler = ConversationHandler(
-    entry_points=[CommandHandler('start', start)],
+    entry_points=[MessageHandler(filters.Regex('start'), start)],
     states={
-        CHOOSING: [],
     },
-    fallbacks=[MessageHandler(filters.Regex(BACK), go_home)],
-    map_to_parent={
-        CHOOSING: CHOOSING,
-        ConversationHandler.END: ConversationHandler.END
-    },
+    fallbacks=[],
     name="main-handler",
     persistent=True,
 )
