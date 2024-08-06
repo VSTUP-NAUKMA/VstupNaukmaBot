c8721d5e12e800c0b6a59b58ca542921fe64e9ca
Yevhen Anisimov <yevhen.anisimov@ukma.edu.ua>
2024-08-06 22:32:31 +0300

    Fixes

--- main.py
+++ main.py
@@ -43,6 +43,6 @@ if __name__ == '__main__':
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.create_task(clear_pending_replies(86400))  # 24 hours
+    loop.create_task(clear_pending_replies(259200))  # 3 days
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
